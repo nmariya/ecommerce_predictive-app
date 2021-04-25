9f1d97f30b52811495762c8f14376661edb8a731
Neethu Mariya <nmariya@uwaterloo.ca>
2021-04-25 19:45:22 -0400

    Updated running code

--- application/app.py
+++ application/app.py
@@ -6,7 +6,21 @@ from nltk.stem import PorterStemmer
 import torch
 import torch.nn as nn
 from torch.autograd import Variable
-# RNN model
+from keras.models import Model, load_model, model_from_json
+import tensorflow as tf
+from keras.initializers import glorot_uniform
+from keras import applications
+from keras.applications.inception_resnet_v2 import InceptionResNetV2
+from keras.layers import Input, Activation, Dropout, Flatten, Dense, BatchNormalization, Conv2D
+from keras.optimizers import Adam
+from keras import metrics
+import numpy as np
+from PIL import Image
+import cv2
+import matplotlib.pyplot as plt
+import warnings
+warnings.filterwarnings("ignore")
+# Text model - RNN with LSTM units
 class RNN_lstm(nn.Module):
     def __init__(self, input_size, hidden_size, output_size):
         super(RNN_lstm, self).__init__()
@@ -26,6 +40,29 @@ class RNN_lstm(nn.Module):
 
     def initHidden(self):
         return torch.zeros(1, self.hidden_size)
+
+# Image model - CNN (InceptionResNetV2)
+def create_model(input_shape, n_out):
+    
+    pretrain_model = InceptionResNetV2(
+        include_top=False, 
+        weights='imagenet', 
+        input_shape=input_shape)    
+    
+    input_tensor = Input(shape=input_shape)
+    bn = BatchNormalization()(input_tensor)
+    x = pretrain_model(bn)
+    x = Conv2D(128, kernel_size=(1,1), activation='relu')(x)
+    x = Flatten()(x)
+    x = Dropout(0.5)(x)
+    x = Dense(512, activation='relu')(x)
+    x = Dropout(0.5)(x)
+    output = Dense(n_out, activation='sigmoid')(x)
+    model = Model(input_tensor, output)
+    
+    return model
+i_model = create_model(input_shape=(299,299,3), n_out=27)
+
 # Dictionary
 with open('./ML_models/train_dictionary.pk', 'rb') as f:
     res2 = pickle.load(f)
@@ -34,20 +71,20 @@ n_words = len(res2)
 with open('./ML_models/cat_dictionary.pk', 'rb') as f:
     all_categories = pickle.load(f)
 n_categories = len(all_categories)
-# Text Classification model
-# with open('textmodel.pk', 'rb') as f:
-#     elmodel = pickle.load(f)
 n_hidden = 128
+# Load the saved model states
 model = RNN_lstm(n_words, n_hidden, n_categories)
 model.load_state_dict(torch.load('./ML_models/char_rnn_lstm_classification_model_state.pt'))
 
+# Load model weights to 
+i_model.load_weights("./ML_models/model.h5")
 app = Flask(__name__)
 
 @app.route('/')
 def main():
     return render_template('index.html')
 
-# @app.route('/preprocessing')
+# Text preprocessing
 def clean_doc(doc):
     '''
     This function will clean the text
@@ -82,16 +119,6 @@ def lineToTensor(line):
         tensor[li][0][wordToIndex(word)] = 1
     return tensor    
 
-
-
-
-
-
-
-test_str = "Women	Brown	Summer	Casual	Senorita ColorBurst Brown Flats	"
-t = clean_doc(test_str)
-line_tensor = lineToTensor(t)
-
 def evaluate_pickl(line_tensor, n_predictions=1):
     '''Prediction is happening here'''
     hidden = model.initHidden()
@@ -104,15 +131,32 @@ def evaluate_pickl(line_tensor, n_predictions=1):
             category_index_lstm = topi_lstm[0][i].item()
     return all_categories[category_index_lstm]
 
-
+# Image preprocessing
+def load_image(path, shape = (299,299,3)):
+    '''This function converts the image to the Image-Net dataset standards'''
+    image = np.array(Image.open(path+'.jpg'))
+    image = cv2.resize(image, (shape[0], shape[1]))
+    image = np.divide(image, 255)
+    return image 
 
 @app.route('/predict')
 def predict():
+    # Text prediction
     test_str = "Women	Brown	Summer	Casual	Senorita ColorBurst Brown Flats	"
     t = clean_doc(test_str)
     line_tensor = lineToTensor(t)
     out = evaluate_pickl(line_tensor)
-    return out
+    return "Prediction from Text Classification model " + out
+
+@app.route('/i-predict')
+def i_predict():
+    # image prediction
+    image = load_image('images/56946')
+    # imgplot = plt.imshow(image)
+    # plt.show()
+    score_predict = i_model.predict(image[np.newaxis])[0]
+    label_predict = np.argmax(score_predict)
+    return "Prediction from Image Classification model " + all_categories[label_predict]
 
 # @app.route('/predict')
 # def evaluate(line_tensor):
@@ -149,4 +193,4 @@ def predict():
 #     return jsonify(Fruits=list_of_fruits, Drinks=list_of_drinks)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=8080 ,debug=True)
